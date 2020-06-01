5f0639fe2eace520856c86f3ad8b4844b11c719e
Pritesh <pritesh.soni@active.ai>
2020-06-01 16:58:39 +0530

    chat-server: modified git ignore

---
+++ src/test/java/com/app/chat/WebsocketSpringBootApplicationTests.java
@@ -0,0 +1,20 @@
+package com.app.chat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author Pritesh Soni
+ *
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebsocketSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
