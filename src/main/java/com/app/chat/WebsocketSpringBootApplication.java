015d5785e59890b2404af66fd1d95adb6bfc6070
Pritesh <pritesh.soni@active.ai>
2020-06-01 17:04:36 +0530

    chat-server: added springboot starter application class

---
+++ src/main/java/com/app/chat/WebsocketSpringBootApplication.java
@@ -0,0 +1,15 @@
+package com.app.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Pritesh Soni
+ *
+ */
+@SpringBootApplication
+public class WebsocketSpringBootApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(WebsocketSpringBootApplication.class, args);
+  }
+}
