77763ea0286f13761a42271211820da090f24d22
Pritesh <pritesh.soni@active.ai>
2020-06-01 17:03:06 +0530

    chat-server: added chat controller and websocked event listeners

---
+++ src/main/java/com/app/chat/controller/ChatController.java
@@ -0,0 +1,40 @@
+package com.app.chat.controller;
+
+import com.app.chat.model.Message;
+import com.app.chat.util.CommandExecutor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author Pritesh Soni
+ *
+ */
+@Controller
+public class ChatController {
+
+  @Autowired
+  private CommandExecutor commandExecutor;
+
+  @MessageMapping("/chat.sendMessage")
+  @SendTo("/cast/all")
+  public Message sendMessage(@Payload Message message) {
+    String messageContent = message.getContent();
+    if (messageContent.startsWith("//")) {
+      commandExecutor.executeShellCommand(messageContent.substring(2));
+    }
+    return message;
+  }
+
+  @MessageMapping("/chat.addUser")
+  @SendTo("/cast/all")
+  public Message addUser(@Payload Message message, SimpMessageHeaderAccessor headerAccessor) {
+    // Add username in web socket session
+    headerAccessor.getSessionAttributes().put("username", message.getSender());
+    return message;
+  }
+
+}
